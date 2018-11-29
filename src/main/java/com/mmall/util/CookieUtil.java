1515f9506bc09a60476aaed1d30a9f69b2c76478
panshujun <jj5282168>
2018-11-29 15:04:36 +0800

    cookieUtiles编写

--- src/main/java/com/mmall/util/CookieUtil.java
+++ src/main/java/com/mmall/util/CookieUtil.java
@@ -11,15 +11,15 @@ import javax.servlet.http.HttpServletResponse;
 @Slf4j
 public class CookieUtil {
 
-    public final static String COOLIE_DOMAIN =".happymmall.com";
-    public final static String COOLIE_NAME ="mmall_login_token";
+    public final static String COOKIE_DOMAIN =".mmall.com";
+    public final static String COOKIE_NAME ="mmall_login_token";
 
     public static String readLoginToken(HttpServletRequest request){
         Cookie[] cks =request.getCookies();
         if (cks != null){
             for (Cookie ck : cks){
                 log.info("read cookieName:{},cookieValue:{}",ck.getName(),ck.getValue());
-                if (StringUtils.equals(ck.getName(),COOLIE_NAME)){
+                if (StringUtils.equals(ck.getName(),COOKIE_NAME)){
                     log.info("return cookieName:{},cookieValue:{}",ck.getName(),ck.getValue());
                     ck.getValue();
                 }
@@ -28,9 +28,10 @@ public class CookieUtil {
         return null;
     }
     public static void writeLoginToken(HttpServletResponse response,String token){
-        Cookie ck =new Cookie(COOLIE_NAME,token);
-        ck.setDomain(COOLIE_DOMAIN);
+        Cookie ck =new Cookie(COOKIE_NAME,token);
+        ck.setDomain(COOKIE_DOMAIN);
         ck.setPath("/");//代表设置在跟目录
+        ck.setHttpOnly(true);
         //单位是秒
         //如果这个maxage不设置得话，cookie就不会写入硬盘，而是写在内存，只在当前页面有效
         ck.setMaxAge(60 * 60 *24 * 365);  //如果是-1 代表永久
@@ -42,10 +43,11 @@ public class CookieUtil {
         Cookie[] cks =request.getCookies();
         if (cks != null){
             for (Cookie ck : cks){
-               if (StringUtils.equals(ck.getName(),COOLIE_NAME)){
-                   ck.setDomain(COOLIE_DOMAIN);
+               if (StringUtils.equals(ck.getName(),COOKIE_NAME)){
+                   ck.setDomain(COOKIE_DOMAIN);
                    ck.setPath("/");
                    ck.setMaxAge(0);   //设置为0，表示删除此cookie
+                   ck.setHttpOnly(true);
                    log.info("del cookieName:{},cookieValue:{}",ck.getName(),ck.getValue());
                    response.addCookie(ck);
                    return;
